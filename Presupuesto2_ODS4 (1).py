9300e27df02901795a39cdb5ca896f16ba5e8eb5
VanessaA01614392 <a01614392@tec.mx>
2026-06-12 08:19:09 -0600

    Update Presupuesto2_ODS4 (1).py

--- Presupuesto2_ODS4 (1).py
+++ Presupuesto2_ODS4 (1).py
@@ -1,4 +1,4 @@
-# Importar librerías necearias
+# Importar librerías necesarias
 import numpy as np
 import streamlit as st
 import pandas as pd
@@ -14,8 +14,13 @@ st.sidebar.header("Presupuesto")
 # Límite inferior: 20000000000
 # Límite superior: 64000000000
 # Valor inicial: 40000000000
-presupuesto = st.sidebar.slider("Presupuesto", 700, 2000, 1500)
-
+presupuesto = st.sidebar.slider("Presupuesto", 10000000, 45000000, 20000000)
+st.sidebar.header("Porcentaje de Becas")
+porcentaje_becas = st.sidebar.slider("Porcentaje de Becas", 0.2, 1.0, 0.5)
+st.sidebar.header("Porcentaje de Infraestructura")
+porcentaje_infra = st.sidebar.slider("Porcentaje de Infraestructura", 0.0, 0.5, 0.25)
+st.sidebar.header("Porcentaje de Docentes")
+porcentaje_docentes = st.sidebar.slider("Porcentaje de Docentes", 0.15, 1.0, 0.35)
 # Cargamos el archivo con los datos (.csv)
 datos = pd.read_csv('India_ODS4 (1).csv', encoding='latin-1')
 # Seleccionamos las variables
@@ -29,6 +34,26 @@ LR.fit(X,y)
 b1 = LR.coef_
 b0 = LR.intercept_
 # Especificamos datos por población
-
-impacto = b0 + prep_alumno*b1[0]
+numero = 28000000
+prep_alumno = presupuesto/numero
+# Calculamos el presupuesto asignado a cada rubro
+presupuesto_becas = presupuesto * porcentaje_becas
+presupuesto_infra = presupuesto * porcentaje_infra
+presupuesto_docentes = presupuesto * porcentaje_docentes
+# Presentamos loa resultados
+total_gastado = presupuesto*(porcentaje_becas + porcentaje_infra +porcentaje_docentes)
+# Validar restricciones
+st.subheader('Impacto alcanzado')
+impacto = b0 + prep_alumno*b1[0] + presupuesto_infra/1000000000*0.15 +presupuesto_docentes/1000000000*.14
 st.metric("Impacto Proyectado ODS 4", f"+{float(impacto):.3f}%")
+# Presentamos el tipo de filosofía
+if porcentaje_becas >= 0.40:
+filosofia = "Bienestar Primero (Equidad y Movilidad Social)"
+elif porcentaje_infra >= 0.45:
+filosofia = "Rendimiento Estructural (Desarrollo Sostenible)"
+elif porcentaje_docentes >= 0.35:
+filosofia = "Efecto Multiplicador (Excelencia Académica)"
+else:
+filosofia = "Gobernanza Equilibrada (Modelo Balanceado)"
+st.subheader("Clasificación Estratégica del Modelo")
+st.info(f"Su propuesta óptima califica como un enfoque de: **{filosofia}**")
